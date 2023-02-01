48b416409d8906b74370d7b07d8609efb6f8a542
yihe4728 <yihe4728@gmail.com>
2023-02-01 15:46:43 +0900

    Feat: model 추가
    Pets, Calender, Walk model 추가

---
+++ dogdack/lib/models/walk_data.dart
@@ -0,0 +1,31 @@
+import "package:cloud_firestore/cloud_firestore.dart";
+
+class WalkData {
+  WalkData({this.name, this.startTime, this.endTime, this.place, this.distance});
+
+  final String? name; // 반려견 이름
+  //좌표 리스트
+  final Timestamp? startTime; // 산책 시작 시간
+  final Timestamp? endTime; // 산책 종료 시간
+  final String? place; // 대표 산책 장소
+  final int? distance; // 이동 거리
+
+  WalkData.fromJson(Map<String, dynamic> json)
+      : this(
+    name: json['name']! as String,
+    startTime: json['startTime']! as Timestamp,
+    endTime: json['endTime']! as Timestamp,
+    place: json['place']! as String,
+    distance: json['distance']! as int,
+  );
+
+  Map<String, dynamic> toJson() {
+    return {
+      'name': name,
+      'startTime': startTime,
+      'endTime': endTime,
+      'place': place,
+      'distance': distance,
+    };
+  }
+}
