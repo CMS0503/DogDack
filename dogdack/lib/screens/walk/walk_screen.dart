7e1b35efc3c686bd40704c71d0b7ce659a1faaab
CMS0503 <tyms0503@gmail.com>
2023-02-05 16:42:39 +0900

    Feat: 산책 종료 화면

--- dogdack/lib/screens/walk/walk_screen.dart
+++ dogdack/lib/screens/walk/walk_screen.dart
@@ -12,6 +12,124 @@ class WalkPage extends StatelessWidget {
 
   final walkController = Get.put(WalkController());
 
+  Widget mapAreaWidget(w, h) {
+    return Container(
+      height: h * 0.65,
+      width: w,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(10),
+        child: myMap(),
+      ),
+    );
+  }
+
+  Widget requestBluetoothConnectWidget(w, h, context) {
+    return Container(
+      height: h * 0.65,
+      width: w,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            '블루투스 연결을 확인해주세요',
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.bluetooth_outlined,
+                color: Colors.blue,
+              ),
+              TextButton(
+                onPressed: () => Navigator.pushNamed(context, '/Ble'),
+                child: Text('지금 연결하러 가기'),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget endWalkModal(w, h, context) {
+    return Opacity(
+      opacity: 0.7,
+      child: Container(
+        decoration: BoxDecoration(color: Colors.grey),
+        height: h * 0.65,
+        width: w,
+        child: Align(
+          alignment: Alignment.center,
+          child: Container(
+            height: 100,
+            width: w * 0.9,
+            decoration: BoxDecoration(
+                color: Colors.white, borderRadius: BorderRadius.circular(15)),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: 10,
+                ),
+                Text('산책하기를 종료합니다'),
+                Text('산책 거리가 짧으면 기록되지 않습니다.'),
+                SizedBox(
+                  height: 5,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      width: w * 0.45,
+                      // decoration: BoxDecoration(color: Colors.red),
+                      child: Align(
+                        alignment: Alignment.center,
+                        child: TextButton(
+                          style: TextButton.styleFrom(
+                              padding: EdgeInsets.zero,
+                              minimumSize: Size(50, 30)),
+                          child: Text(
+                            '산책 계속하기',
+                            style: Theme.of(context).textTheme.bodyMedium,
+                          ),
+                          onPressed: () {
+                            walkController.updateWalkingState();
+                            walkController.startTimer();
+                          },
+                        ),
+                      ),
+                    ),
+                    Container(
+                      width: w * 0.45,
+                      // decoration: BoxDecoration(color: Colors.blue),
+                      child: Align(
+                        alignment: Alignment.center,
+                        child: TextButton(
+                          child: Text(
+                            '종료',
+                            style: TextStyle(color: Colors.red, fontSize: 16),
+                          ),
+                          onPressed: () => {},
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 5,
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -32,104 +150,15 @@ class WalkPage extends StatelessWidget {
             SizedBox(height: 10),
             Status(),
             SizedBox(height: 10),
-            walkController.isBleConnect == false
-                ? Container(
-                    height: screenHeight * 0.65,
-                    width: screenWidth,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          '블루투스 연결을 확인해주세요',
-                          style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.w500,
-                          ),
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Icon(
-                              Icons.bluetooth_outlined,
-                              color: Colors.blue,
-                            ),
-                            TextButton(
-                              onPressed: () =>
-                                  Navigator.pushNamed(context, '/Ble'),
-                              child: Text('지금 연결하러 가기'),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  )
+            walkController.isBleConnect.value == false
+                ? requestBluetoothConnectWidget(
+                    screenWidth, screenHeight, context)
                 : Stack(
                     children: [
-                      Container(
-                        height: screenHeight * 0.65,
-                        width: screenWidth,
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(10),
-                          child: myMap(),
-                        ),
-                      ),
-                      (walkController.isRunning == walkController.isStart)
+                      mapAreaWidget(screenWidth, screenHeight),
+                      (walkController.isRunning.value == walkController.isStart)
                           ? Container()
-                          : Opacity(
-                              opacity: 0.7,
-                              child: Container(
-                                decoration: BoxDecoration(color: Colors.grey),
-                                height: screenHeight * 0.65,
-                                width: screenWidth * 1.2,
-                                child: Align(
-                                  alignment: Alignment.center,
-                                  child: Container(
-                                    height: 100,
-                                    width: screenWidth * 0.9,
-                                    decoration: BoxDecoration(
-                                        color: Colors.white,
-                                        borderRadius:
-                                            BorderRadius.circular(15)),
-                                    child: Column(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.center,
-                                      children: [
-                                        Text('산책하기를 종료합니다'),
-                                        Text('산책 거리가 짧으면 기록되지 않습니다.'),
-                                        SizedBox(
-                                          height: 10,
-                                        ),
-                                        Row(
-                                          mainAxisAlignment:
-                                              MainAxisAlignment.center,
-                                          children: [
-                                            Container(
-                                              decoration: BoxDecoration(
-                                                  color: Colors.red),
-                                              child: TextButton(
-                                                child: Text('산책 계속하기'),
-                                                onPressed: () {
-                                                  walkController
-                                                      .updateWalkingState();
-                                                  walkController.startTimer();
-                                                },
-                                              ),
-                                            ),
-                                            Container(
-                                                decoration: BoxDecoration(
-                                                    color: Colors.blue),
-                                                child: Text('종료')),
-                                          ],
-                                        ),
-                                        SizedBox(
-                                          height: 5,
-                                        )
-                                      ],
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            )
+                          : endWalkModal(screenWidth, screenHeight, context),
                     ],
                   ),
           ],
