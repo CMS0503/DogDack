8fef47b1786ca1e4052be0c87f6e083a10aecdae
CMS0503 <tyms0503@gmail.com>
2023-02-05 11:06:27 +0900

    Refactor: padding 추가

--- dogdack/lib/screens/walk/widgets/status.dart
+++ dogdack/lib/screens/walk/widgets/status.dart
@@ -12,64 +12,67 @@ class Status extends StatefulWidget {
 class _StatusState extends State<Status> {
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Row(
-          children: [
-            Text('picture'),
-            SizedBox(
-              width: 20,
-            ),
-            Text('name'),
-            IconButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/Ble');
-                },
-                icon: Icon(Icons.bluetooth_outlined)),
-          ],
-        ),
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                Text(
-                  '권장 산책 시간',
-                  style: Theme.of(context).textTheme.bodyMedium,
-                ),
-                SizedBox(
-                  height: 3,
-                ),
-                Text(
-                  '1시간',
-                  style: Theme.of(context).textTheme.displayMedium,
-                ),
-              ],
-            ),
-            SizedBox(
-              height: 8,
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                Text(
-                  '목표 산책 달성량',
-                  style: Theme.of(context).textTheme.bodyMedium,
-                ),
-                SizedBox(
-                  height: 3,
-                ),
-                Text(
-                  '80%',
-                  style: Theme.of(context).textTheme.displayMedium,
-                ),
-              ],
-            )
-          ],
-        ),
-      ],
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: [
+              Text('picture'),
+              SizedBox(
+                width: 20,
+              ),
+              Text('name'),
+              IconButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/Ble');
+                  },
+                  icon: Icon(Icons.bluetooth_outlined)),
+            ],
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Text(
+                    '권장 산책 시간',
+                    style: Theme.of(context).textTheme.bodyMedium,
+                  ),
+                  SizedBox(
+                    height: 3,
+                  ),
+                  Text(
+                    '1시간',
+                    style: Theme.of(context).textTheme.displayMedium,
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 8,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Text(
+                    '목표 산책 달성량',
+                    style: Theme.of(context).textTheme.bodyMedium,
+                  ),
+                  SizedBox(
+                    height: 3,
+                  ),
+                  Text(
+                    '80%',
+                    style: Theme.of(context).textTheme.displayMedium,
+                  ),
+                ],
+              )
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
