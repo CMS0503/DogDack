a7f03196371cf72a207015b9727e5f1381b09f58
CMS <tyms0503@gmail.com>
2023-02-06 21:34:57 +0900

    Refactor: 하단탭바 인덱스 getx로 변경
    어디서는 탭변경이 쉽게 하기 위해 변경함

--- dogdack/lib/screens/main_screen.dart
+++ dogdack/lib/screens/main_screen.dart
@@ -1,4 +1,6 @@
+import 'package:dogdack/controlls/main_controll.dart';
 import 'package:dogdack/navigators/calender_navigator.dart';
+
 //navigator
 import 'package:dogdack/navigators/home_navigator.dart';
 import 'package:dogdack/navigators/mypage_navigator.dart';
@@ -6,74 +8,68 @@ import 'package:dogdack/navigators/walk_navigator.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class MainPage extends StatefulWidget {
-  const MainPage({Key? key}) : super(key: key);
+import 'package:get/get.dart';
 
-  @override
-  State<MainPage> createState() => _MainPageState();
-}
+class MainPage extends StatelessWidget {
+  MainPage({Key? key}) : super(key: key);
 
-class _MainPageState extends State<MainPage> {
+  final mainController = Get.put(MainController());
+
   int _currentTabIndex = 0;
 
-  void _tabSelect(int tabIndex) {
-    setState(() {
-      _currentTabIndex = tabIndex;
-    });
-  }
-
+  // void _tabSelect(int tabIndex) {
   @override
   Widget build(BuildContext context) {
     FirebaseAuth.instance.currentUser;
 
-    return Scaffold(
-      body: Stack(children: [
-        Offstage(
-          offstage: _currentTabIndex != 0,
-          child: const HomeNavigator(tabIndex: 0),
-        ),
-        Offstage(
-          offstage: _currentTabIndex != 1,
-          child: const CalenderNavigator(tabIndex: 1),
-        ),
-        Offstage(
-          offstage: _currentTabIndex != 2,
-          child: WalkNavigator(tabIndex: 2),
-        ),
-        Offstage(
-          offstage: _currentTabIndex != 3,
-          child: const MyPageNavigator(tabIndex: 3),
-        ),
-      ]),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: '홈',
+    return Obx(() => Scaffold(
+          body: Stack(children: [
+            Offstage(
+              offstage: mainController.tabindex != 0,
+              child: const HomeNavigator(tabIndex: 0),
+            ),
+            Offstage(
+              offstage: mainController.tabindex != 1,
+              child: const CalenderNavigator(tabIndex: 1),
+            ),
+            Offstage(
+              offstage: mainController.tabindex != 2,
+              child: WalkNavigator(tabIndex: 2),
+            ),
+            Offstage(
+              offstage: mainController.tabindex != 3,
+              child: const MyPageNavigator(tabIndex: 3),
+            ),
+          ]),
+          bottomNavigationBar: BottomNavigationBar(
+            items: const [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                label: '홈',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.calendar_today),
+                label: '캘린더',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.pets_outlined),
+                label: '산책',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.person),
+                label: '마이',
+              ),
+            ],
+            selectedItemColor: const Color.fromARGB(255, 100, 92, 170),
+            type: BottomNavigationBarType.fixed,
+            backgroundColor: Colors.white,
+            unselectedLabelStyle: const TextStyle(fontFamily: 'bmjua'),
+            selectedLabelStyle: const TextStyle(fontFamily: 'bmjua'),
+            elevation: 0,
+            unselectedItemColor: Colors.grey,
+            currentIndex: mainController.tabindex,
+            onTap: (value) => mainController.changeTabIndex(value),
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.calendar_today),
-            label: '캘린더',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.pets_outlined),
-            label: '산책',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: '마이',
-          ),
-        ],
-        selectedItemColor: const Color.fromARGB(255, 100, 92, 170),
-        type: BottomNavigationBarType.fixed,
-        backgroundColor: Colors.white,
-        unselectedLabelStyle: const TextStyle(fontFamily: 'bmjua'),
-        selectedLabelStyle: const TextStyle(fontFamily: 'bmjua'),
-        elevation: 0,
-        unselectedItemColor: Colors.grey,
-        currentIndex: _currentTabIndex,
-        onTap: (value) => _tabSelect(value),
-      ),
-    );
+        ));
   }
 }
