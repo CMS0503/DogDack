e9bd7e217210b3c9433516fe85e26ae6834a33ca
youngwoo2020@naver.com <youngwoo2020@naver.com>
2023-02-09 21:33:05 +0900

    "chart 계산 완료"

--- dogdack/lib/screens/chart/chart_screen.dart
+++ dogdack/lib/screens/chart/chart_screen.dart
@@ -15,7 +15,6 @@ import '../../controllers/mypage_controller.dart';
 class ChartMain extends StatefulWidget {
   static Map<String, List> events = {'': []};
 
-
   @override
   State<ChartMain> createState() => _ChartMainState();
 }
@@ -29,17 +28,24 @@ class _ChartMainState extends State<ChartMain> {
 // 캘린더에서 받아온 데이터
   String docId = '짬뽕';
 
-
   ////////////////////////////////////////////파이어 베이스 연결 데이터/////////////////////////////////////////
 
 //////////////////////////////////////////시간 관련 변수//////////////////////////////////////////
   // 일주일 산책 시간 그래프 포인트
   late List<double> day_hour_points = [1];
+
+  //저번주 산책 시간 그래프
+  late List<double> last_day_hour_points = [1];
+
   // 일주일 산책 목표 시간 포인트
   late List<double> day_goal_hour_points = [1];
 
   // 한달 산책 시간 그래프 포인트
   late List<double> week_hour_points = [1];
+
+  // 저번달 산책 시간 그래프 포인트
+  late List<double> last_week_hour_points = [1];
+
   //한달 산책 목표 시간 포인트
   late List<double> week_goal_hour_points = [1];
 
@@ -67,7 +73,6 @@ class _ChartMainState extends State<ChartMain> {
   // 저번달 목표 산잭 시간
   int last_week_goal_hour = 1;
 
-
   //산책 시간 포인트
   late List<double> hour_points = [1, 1, 1, 1, 1, 1, 1];
 
@@ -90,28 +95,26 @@ class _ChartMainState extends State<ChartMain> {
   late int last_hour_goal_data = 1;
 
   // this 산책 목표 달성률
-  late int walk_goal_data =1;
+  // late int walk_goal_data = 1;
 
   // last 산책 목표 달성률
   late int last_walk_goal_data =
-  ((walk_hour_data / hour_goal_data) * 100).toInt();
+      ((walk_hour_data / hour_goal_data) * 100).toInt();
 
   // 산책 목표 달성률 증감
-  late int walk_goal_increment =1;
+  late int walk_goal_increment = 1;
 
   // 산책 목표 달성률 증감 표시 텍스트
   late String walk_goal_increment_text = "";
   String walk_goal_plus = "올랐어요!";
   String walk_goal_minus = "떨어졌어요!";
 
-  //산책 시간 증감
-  late int hour_increment = walk_hour_data - last_walk_hour_data;
+
 
   // 산책 시간 증감 표시 텍스트
   late String walk_hour_increment_text = "";
   String walk_hour_plus = "늘었어요!";
   String walk_hour_minus = "줄었어요!";
-
 
   ///////////////////////////////////////////////////거리 관련 변수////////////////////////////////////
 
@@ -120,7 +123,6 @@ class _ChartMainState extends State<ChartMain> {
 
   // 한달 산책 시간 그래프 포인트
   late List<double> week_distance_points = [1];
-
 
   // 이번주 일주일 평균 산책 거리
   int sum_day_walk_distance = 1;
@@ -134,7 +136,6 @@ class _ChartMainState extends State<ChartMain> {
   // 저번달 평균 산책 거리
   int last_sum_week_walk_distance = 1;
 
-
   late String imageUrl = 'images/login/login_image.png';
 
   // 드롭박스 값
@@ -146,23 +147,18 @@ class _ChartMainState extends State<ChartMain> {
   Widget x_value_day = DayWidget();
   Widget x_value_week = WeekWidget();
 
-
   // this 산책 거리
   late int walk_distance_data = 1;
 
   // last 산책 거리
   late int last_walk_distance_data = 1;
 
-  //산책 거리 증감
-  late int distance_increment = walk_distance_data - last_walk_distance_data;
+
 
   // 산책 거리 증감 표시 텍스트
   late String walk_distance_increment_text = "";
   String walk_distance_plus = "증가했어요!";
   String walk_distance_minus = "감소했어요!";
-
-
-
 
   Future<Map<String, List<Object>>> getData() async {
     // 두달 산책 시간 포인트 불러오기
@@ -170,9 +166,10 @@ class _ChartMainState extends State<ChartMain> {
         .collection('Users/${FirebaseAuth.instance.currentUser!.email}/Pets')
         .doc(docId)
         .collection('Walk')
-        .where('startTime', isLessThan: controller.today)
+        .where('startTime', isLessThan: DateTime.now())
         .where('startTime',
-        isGreaterThan: controller.today.subtract(Duration(days: 60))).orderBy('startTime',  descending: false);
+            isGreaterThan:DateTime.now().subtract(Duration(days: 60)))
+        .orderBy('startTime', descending: false);
 
     var points_result = await day_points.get();
 
@@ -190,32 +187,51 @@ class _ChartMainState extends State<ChartMain> {
           .substring(0, 10));
     }
 
-    List<double> temp = List<double>.filled(60, 1);
-    day_hour_points = List<double>.filled(7, 1);
-    week_hour_points = List<double>.filled(30, 1);
-    day_distance_points = List<double>.filled(7, 1);
-    week_distance_points =List<double>.filled(30, 1);
-    day_goal_hour_points =  List<double>.filled(7, 1);
-    week_goal_hour_points =  List<double>.filled(30, 1);
+    day_hour_points = List<double>.filled(7, 0);
+    week_hour_points = List<double>.filled(30, 0);
+
+    last_day_hour_points= List<double>.filled(7, 0);
+    last_week_hour_points = List<double>.filled(30, 0);
+
+    day_distance_points = List<double>.filled(7, 0);
+    week_distance_points = List<double>.filled(30, 0);
+
+    day_goal_hour_points = List<double>.filled(7, 0);
+    week_goal_hour_points = List<double>.filled(30, 0);
 
 
+
+    List<double> two_month_hour = List<double>.filled(60, 0);
+    List<double> two_month_distance = List<double>.filled(60, 0);
+    List<double> two_month_goal = List<double>.filled(60, 0);
 
     for (int i = 0; i < date_list.length; i++) {
       for (int j = 0; j < docs_list.length; j++) {
         if (docs_list[j] == (date_list[i])) {
-          if(i>60-7){
-            day_hour_points[date_list.length-i] = points_result.docs[j]['totalTimeMin'].toDouble();
-            day_distance_points[date_list.length-i] = points_result.docs[j]['distance'].toDouble();
-            day_goal_hour_points[date_list.length-i] = points_result.docs[j]['goal'].toDouble();
-          }
-          if(i>60-30){
-            week_hour_points[date_list.length-i] = points_result.docs[j]['totalTimeMin'].toDouble();
-            week_distance_points[date_list.length-i] = points_result.docs[j]['distance'].toDouble();
-            week_goal_hour_points[date_list.length-i] = points_result.docs[j]['goal'].toDouble();
-          }
+          two_month_hour[i] = points_result.docs[j]['totalTimeMin'].toDouble();
+          two_month_distance[i] = points_result.docs[j]['distance'].toDouble();
+          two_month_goal[i] = points_result.docs[j]['goal'].toDouble();
         }
       }
     }
+
+
+    day_hour_points = two_month_hour.sublist(two_month_hour.length-7, two_month_hour.length);
+    week_hour_points= two_month_hour.sublist(two_month_hour.length-30, two_month_hour.length);
+
+
+
+    last_day_hour_points = two_month_hour.sublist(two_month_distance.length-14, two_month_distance.length-7);
+    last_week_hour_points= two_month_hour.sublist(0, two_month_distance.length-30);
+
+
+    day_distance_points = two_month_distance.sublist(two_month_distance.length-7, two_month_distance.length);
+    week_distance_points = two_month_distance.sublist(two_month_distance.length-30, two_month_distance.length);
+
+
+    day_goal_hour_points = two_month_goal.sublist(two_month_goal.length-7, two_month_goal.length);
+    week_goal_hour_points = two_month_goal.sublist(two_month_goal.length-30, two_month_goal.length);
+
 
     return events;
   }
@@ -228,9 +244,9 @@ class _ChartMainState extends State<ChartMain> {
 
   ////////////////////////////////////파이어 베이스 연결 끝/////////////////////////////////////////////////////
 
-
   @override
   Widget build(BuildContext context) {
+
 
     Size screenSize = MediaQuery.of(context).size;
     double width = screenSize.width;
@@ -242,32 +258,66 @@ class _ChartMainState extends State<ChartMain> {
     // 일주일 동안 실제 산책한 평균 시간
     for (int i = 0; i < day_hour_points.length; i++) {
       sum_day_walk_hour += day_hour_points[i].toInt();
+      last_sum_day_walk_hour += last_day_hour_points[i].toInt();
       sum_day_walk_distance += day_distance_points[i].toInt();
       day_goal_hour += day_goal_hour_points[i].toInt();
+
     }
     sum_day_walk_hour = (sum_day_walk_hour / day_hour_points.length).toInt();
-    sum_day_walk_distance = (sum_day_walk_distance / day_distance_points.length).toInt();
+    last_sum_day_walk_hour= (last_sum_day_walk_hour/last_day_hour_points.length).toInt();
+    sum_day_walk_distance =
+        (sum_day_walk_distance / day_distance_points.length).toInt();
     day_goal_hour = (day_goal_hour / day_goal_hour_points.length).toInt();
+
+
 
     // 한달 동안 실제 산책한 평균 시간
     for (int i = 0; i < week_hour_points.length; i++) {
       sum_week_walk_hour += week_hour_points[i].toInt();
+      last_sum_week_walk_hour += last_week_hour_points[i].toInt();
       sum_week_walk_distance += week_distance_points[i].toInt();
       week_goal_hour += week_goal_hour_points[i].toInt();
     }
     sum_week_walk_hour = (sum_week_walk_hour / week_hour_points.length).toInt();
-    sum_week_walk_distance = (sum_week_walk_distance / week_distance_points.length).toInt();
+    last_sum_week_walk_hour = (last_sum_week_walk_hour / last_week_hour_points.length).toInt();
+    sum_week_walk_distance =
+        (sum_week_walk_distance / week_distance_points.length).toInt();
     week_goal_hour = (week_goal_hour / week_distance_points.length).toInt();
-    hour_increment = walk_hour_data - last_walk_hour_data;
-    distance_increment = walk_distance_data - last_walk_distance_data;
-    walk_goal_data =
-        ((walk_hour_data / hour_goal_data) * 100).toInt();
-    walk_goal_increment =
-        (walk_goal_data - last_walk_goal_data).toInt();
+
+
+    //산책 시간 증감
+    int hour_increment = walk_hour_data - last_walk_hour_data;
+    int distance_increment = walk_distance_data - last_walk_distance_data;
+    int walk_goal_data = ((walk_hour_data / hour_goal_data) * 100).toInt();
+    print(hour_goal_data);
+    print(walk_hour_data);
+    int walk_goal_increment = (walk_goal_data - last_walk_goal_data).toInt();
+
+
+    // 각각 증감 텍스트 변경
+    if (walk_goal_increment > 0) {
+      walk_goal_increment_text = walk_goal_plus;
+    } else {
+      walk_goal_increment_text = walk_goal_minus;
+    }
+
+    if (hour_increment > 0) {
+      walk_hour_increment_text = walk_hour_plus;
+    } else {
+      walk_hour_increment_text = walk_hour_minus;
+    }
+
+    if (distance_increment > 0) {
+      walk_distance_increment_text =
+          walk_distance_plus;
+    } else {
+      walk_distance_increment_text =
+          walk_distance_minus;
+    }
 
 
     return Scaffold(
-      appBar:  PreferredSize(
+      appBar: PreferredSize(
         preferredSize: Size.fromHeight(height * 0.09),
         child: const LogoWidget(),
       ),
@@ -284,95 +334,62 @@ class _ChartMainState extends State<ChartMain> {
                     padding: const EdgeInsets.all(3.0),
                     child: Center(
                         child: DropdownButton(
-                          elevation: 0,
-                          focusColor: Color.fromARGB(255, 100, 92, 170),
-                          borderRadius: BorderRadius.circular(10),
-                          value: _selectedValue,
-                          items: _valueList.map((value) {
-                            return DropdownMenuItem(
-                              value: value,
-                              child: Container(
-                                width: width * 0.2,
-                                decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(10),
-                                    border: Border.all(
-                                      width: 2,
-                                      color: Color.fromARGB(255, 100, 92, 170),
-                                    )),
-                                child: Text(
-                                  value,
-                                  style: TextStyle(
-                                      fontSize: 20,
-                                      fontFamily: 'bmjua',
-                                      color: Color.fromARGB(255, 80, 78, 91)),
-                                  textAlign: TextAlign.center,
-                                ),
-                              ),
-                            );
-                          }).toList(),
-                          onChanged: (value) {
-                            setState(
-                                  () {
-                                _selectedValue = value!;
-                                if (_selectedValue == '일주일') {
-                                  hour_points = day_hour_points;
-                                  walk_hour_data = sum_day_walk_hour;
-                                  walk_distance_data = sum_day_walk_distance;
-                                  distance_points = day_distance_points;
-                                  //   last_avg_distance = last_day_avg_distance;
-                                  date_text = "주";
-                                  x_value = x_value_day;
-                                  // 각각 증감 텍스트 변경
-                                  if (walk_goal_increment > 0) {
-                                    walk_goal_increment_text = walk_goal_plus;
-                                  } else {
-                                    walk_goal_increment_text = walk_goal_minus;
-                                  }
+                      elevation: 0,
+                      focusColor: Color.fromARGB(255, 100, 92, 170),
+                      borderRadius: BorderRadius.circular(10),
+                      value: _selectedValue,
+                      items: _valueList.map((value) {
+                        return DropdownMenuItem(
+                          value: value,
+                          child: Container(
+                            width: width * 0.2,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                border: Border.all(
+                                  width: 2,
+                                  color: Color.fromARGB(255, 100, 92, 170),
+                                )),
+                            child: Text(
+                              value,
+                              style: TextStyle(
+                                  fontSize: 20,
+                                  fontFamily: 'bmjua',
+                                  color: Color.fromARGB(255, 80, 78, 91)),
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                        );
+                      }).toList(),
+                      onChanged: (value) {
+                        setState(
+                          () {
+                            _selectedValue = value!;
+                            if (_selectedValue == '일주일') {
+                              hour_points = day_hour_points;
+                              walk_hour_data = sum_day_walk_hour;
+                              last_walk_hour_data = last_sum_day_walk_hour;
+                              walk_distance_data = sum_day_walk_distance;
+                              distance_points = day_distance_points;
+                              //   last_avg_distance = last_day_avg_distance;
+                              date_text = "주";
+                              x_value = x_value_day;
+                              hour_goal_data = day_goal_hour;
 
-                                  if (hour_increment > 0) {
-                                    walk_hour_increment_text = walk_hour_plus;
-                                  } else {
-                                    walk_hour_increment_text = walk_hour_minus;
-                                  }
+                            } else {
+                              hour_points = week_hour_points;
+                              walk_hour_data = sum_week_walk_hour;
+                              last_walk_hour_data = last_sum_week_walk_hour;
+                              walk_distance_data = sum_week_walk_distance;
+                              distance_points = week_distance_points;
+                              date_text = "달";
+                              x_value = x_value_week;
+                              hour_goal_data = week_goal_hour;
 
-                                  if (distance_increment > 0) {
-                                    walk_distance_increment_text =
-                                        walk_distance_plus;
-                                  } else {
-                                    walk_distance_increment_text =
-                                        walk_distance_minus;
-                                  }
-                                } else {
-                                  hour_points = week_hour_points;
-                                  walk_hour_data = sum_week_walk_hour;
-                                  walk_distance_data = sum_week_walk_distance;
-                                  distance_points = week_distance_points;
-                                  date_text = "달";
-                                  x_value = x_value_week;
-
-                                  // 각각 증감 텍스트 변경
-                                  if (walk_goal_increment > 0) {
-                                    walk_goal_increment_text = walk_goal_plus;
-                                  } else {
-                                    walk_goal_increment_text = walk_goal_minus;
-                                  }
-                                  if (hour_increment > 0) {
-                                    walk_hour_increment_text = walk_hour_plus;
-                                  } else {
-                                    walk_hour_increment_text = walk_hour_minus;
-                                  }
-                                  if (distance_increment > 0) {
-                                    walk_distance_increment_text =
-                                        walk_distance_plus;
-                                  } else {
-                                    walk_distance_increment_text =
-                                        walk_distance_minus;
-                                  }
-                                }
-                              },
-                            );
+                            }
                           },
-                        )),
+                        );
+                      },
+                    )),
                   ),
                 ]),
             // Obx(() => Text('${drop_controller.drop_value.value}'),
@@ -382,16 +399,16 @@ class _ChartMainState extends State<ChartMain> {
             // )
             //건강지수 카드
             CalHealthProgressCardWidget(
-                last_data: walk_goal_increment,
+                last_data: walk_goal_increment.abs(),
                 this_data: walk_goal_data,
-                message: "${walk_goal_increment_text}",
+                message: walk_goal_increment_text,
                 date_text: date_text),
             CalHealthCardWidget(
               color: violet2,
               message: "${walk_hour_increment_text}",
               title: "평균 산책 시간",
               points: hour_points,
-              last_data: hour_increment,
+              last_data: hour_increment.abs(),
               this_data: walk_hour_data,
               date_text: date_text,
               unit: "분",
@@ -402,17 +419,15 @@ class _ChartMainState extends State<ChartMain> {
               message: "${walk_distance_increment_text}",
               title: "평균 산책거리",
               points: distance_points,
-              last_data: distance_increment,
+              last_data: distance_increment.abs(),
               this_data: walk_distance_data,
               date_text: date_text,
               unit: "미터",
               x_value: x_value,
             ),
-
           ],
         ),
       ),
     );
   }
 }
-
