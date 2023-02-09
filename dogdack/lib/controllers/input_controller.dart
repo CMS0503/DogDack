583b2147d48d783c1107e7227436d727962585fb
unknown <1109ssh@naver.com>
2023-02-09 20:27:35 +0900

    final merge

--- dogdack/lib/controllers/input_controller.dart
+++ dogdack/lib/controllers/input_controller.dart
@@ -1,18 +1,24 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:get/get.dart';
 
 class InputController extends GetxController {
   DateTime date = DateTime.now();
   String place = '';
-  String time = '';
-  String distance = '';
+  Timestamp startTime = Timestamp.now();
+  Timestamp endTime = Timestamp.now();
+  String distance = '0';
   String diary = '';
+  bool walkCheck = true;
   bool bath = true;
   bool beauty = true;
   List<String> imageUrl = [];
-  DateTime today = DateTime.now();
+  String name = '';
+  List<String> dognames = [];
+  List<String> valueList = [];
+  String selectedValue = '';
+  String saveName = '';
 
-  void setDate(selectedDate) {
-    date = selectedDate;
-    update();
-  }
+  // void input() {
+  //   update();
+  // }
 }
