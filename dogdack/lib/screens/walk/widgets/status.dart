7465fed050b3b8cf0fa02baba50a6e83b5f4ff75
CMS <tyms0503@gmail.com>
2023-02-13 21:55:28 +0900

    Design: 상태바 디자인 개선
    상태바 Card, 항목 컬러화

--- dogdack/lib/screens/walk/widgets/status.dart
+++ dogdack/lib/screens/walk/widgets/status.dart
@@ -4,6 +4,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:circular_profile_avatar/circular_profile_avatar.dart';
+import 'package:dogdack/models/dog_data.dart';
 
 import '../../../controllers/mypage_controller.dart';
 
@@ -46,111 +47,148 @@ class _StatusState extends State<Status> {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     walkController.getCur();
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 10),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Row(
-            children: [
-              Column(
-                children: [
-                  Container(
-                    width: size.width * 0.2,
-                    height: size.width * 0.2,
-                    child: Stack(
-                      fit: StackFit.expand,
-                      children: [
-                        CircleAvatar(
-                          backgroundImage: AssetImage('assets/dog.jpg'),
-                        ),
-                        Positioned(
-                          bottom: 37,
-                          right: 40,
-                          child: IconButton(
-                            onPressed: () {
-                              walkController.ledSig = !walkController.ledSig;
-                            },
-                            icon: Icon(
-                              Icons.lightbulb_outline,
-                              color: Colors.yellow,
-                            ),
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(16.0),
+      ),
+      elevation: 2.0,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            // SizedBox(height: 5,),
+            Row(
+              children: [
+                Column(
+                  children: [
+                    Container(
+                      width: size.width * 0.2,
+                      height: size.width * 0.2,
+                      child: Stack(
+                        fit: StackFit.expand,
+                        children: [
+                          CircleAvatar(
+                            backgroundImage: AssetImage('assets/dog.jpg'),
                           ),
-                        )
-                      ],
+                          Positioned(
+                            bottom: 37,
+                            right: 40,
+                            child: IconButton(
+                              onPressed: () {
+                                walkController.ledSig = !walkController.ledSig;
+                              },
+                              icon: Icon(
+                                Icons.lightbulb_outline,
+                                color: Colors.yellow,
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
                     ),
-                  ),
-                ],
-              ),
-              SizedBox(
-                width: 10,
-              ),
-              Column(
-                children: [
-                  walkController.isBleConnect == true
-                      ? IconButton(
-                          onPressed: () {
-                            Navigator.pushNamed(context, '/Ble');
-                          },
-                          icon: Icon(Icons.bluetooth_connected))
-                      : IconButton(
-                          onPressed: () {
-                            Navigator.pushNamed(context, '/Ble');
-                          },
-                          icon: Icon(Icons.bluetooth_outlined),
+                  ],
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Column(
+                  children: [
+                    walkController.isBleConnect == true
+                        ? IconButton(
+                            onPressed: () {
+                              Navigator.pushNamed(context, '/Ble');
+                            },
+                            icon: Icon(Icons.bluetooth_connected))
+                        : IconButton(
+                            onPressed: () {
+                              Navigator.pushNamed(context, '/Ble');
+                            },
+                            icon: Icon(Icons.bluetooth_outlined),
+                          ),
+                    Text('${walkController.name}'),
+                  ],
+                ),
+              ],
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Container(
+                      width: size.width * 0.3,
+                      height: size.height * 0.035,
+                      decoration: BoxDecoration(
+                          color: Color.fromARGB(255, 221, 137, 189),
+                          borderRadius: BorderRadius.circular(20)),
+                      child: Center(
+                        child: Text(
+                          "오늘의 목표 시간",
+                          style: TextStyle(
+                              fontSize: 16,
+                              fontFamily: 'bmjua',
+                              color: Colors.white),
                         ),
-                  Text('${walkController.name}'),
-                ],
-              ),
-            ],
-          ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  Text(
-                    '목표 산책 시간',
-                    style: Theme.of(context).textTheme.bodyMedium,
-                  ),
-                  const SizedBox(
-                    height: 3,
-                  ),
-                  Obx(
-                    () => Text(
-                      walkController.goal == 0
-                          ? "목표 산책 시간을 입력해 주세요"
-                          : '${walkController.goal} 분',
-                      style: Theme.of(context).textTheme.displayMedium,
+                      ),
                     ),
-                  )
-                ],
-              ),
-              const SizedBox(
-                height: 8,
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  Text(
-                    '목표 산책 달성률',
-                    style: Theme.of(context).textTheme.bodyMedium,
-                  ),
-                  const SizedBox(
-                    height: 3,
-                  ),
-                  Obx(
-                    () => Text(
-                      '${walkController.getCur()} %',
-                      style: Theme.of(context).textTheme.displayMedium,
+                    const SizedBox(
+                      height: 3,
                     ),
-                  )
-                ],
-              )
-            ],
-          ),
-        ],
+                    Obx(
+                      () => Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 7),
+                        child: Text(
+                          walkController.goal == 0
+                              ? "0 분"
+                              : '${walkController.goal} 분',
+                          style: Theme.of(context).textTheme.displayMedium,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+                const SizedBox(
+                  height: 8,
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Container(
+                      width: size.width * 0.3,
+                      height: size.height * 0.035,
+                      decoration: BoxDecoration(
+                          color: Color.fromARGB(255, 160, 132, 202),
+                          borderRadius: BorderRadius.circular(20)),
+                      child: Center(
+                        child: Text(
+                          '목표 산책 달성률',
+                          style: TextStyle(
+                              fontSize: 16,
+                              fontFamily: 'bmjua',
+                              color: Colors.white),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 3,
+                    ),
+                    Obx(
+                      () => Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 7),
+                        child: Text(
+                          '${walkController.getCur()} %',
+                          style: Theme.of(context).textTheme.displayMedium,
+                        ),
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
